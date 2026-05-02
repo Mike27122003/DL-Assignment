824d1b9da89ec94f755191046fa700a1179a45e0
Mike27122003 <dummyraheem@gmail.com>
2026-05-02 09:50:29 +0200

    Add LSTM + bugfix RNN

--- Assign1/model.py
+++ Assign1/model.py
@@ -16,10 +16,33 @@ class RNN(nn.Module):
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
         self.device = device
-        self.rnn = nn.RNN(input_dim, hidden_dim, device=self.device)
+        self.rnn = nn.RNN(input_dim, hidden_dim, batch_first=True, device=self.device)
         self.pred_head = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x: torch.Tensor):
         out, _ = self.rnn(x)
+        pred = self.pred_head(out[:, -1, :])
+        return pred
+
+class LSTM(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        hidden_dim,
+        output_dim,
+        layer_dim=1,
+        device="cpu",
+    ):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.device = device
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, device=self.device)
+        self.pred_head = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x: torch.Tensor):
+        out, (h_n, c_n) = self.lstm(x) # out.shape: (batch_size, seq_len, hidden_dim)
+
         pred = self.pred_head(out[:, -1, :])
         return pred

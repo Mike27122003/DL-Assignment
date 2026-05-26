d8ad197b067fcaf7cd3a81fc419c8bc115f08df3
baguetten <liam.battini9@gmail.com>
2026-05-26 20:48:16 +0200

    changed so everything can be run from main

---
+++ Assign2/src/cnn_methods.py
@@ -0,0 +1,100 @@
+import h5py as h5
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from CNN import CNN
+import torch.nn as nn
+from torch import tensor
+from torchmetrics.classification import Accuracy
+
+#Loads the actual data from 1 training file
+def load_single_data(filename_path):
+    with h5.File(filename_path, "r") as f:
+        key = list(f.keys())[0]
+        matrix = f[key][()]
+        return matrix
+
+#Iterates over all training files (Only Intra for now)
+def load_all_data(FOLDER, window_size, stride):
+    files = list(FOLDER.glob("*.h5"))
+    X_all = []
+    y_all = []
+
+    for file in files:
+        filename = str(file)
+        matrix = load_single_data(filename)
+        windows = create_window(matrix, window_size, stride)
+        label = get_label(filename)
+        y = np.full(len(windows), label)
+        X_all.append(windows)
+        y_all.append(y)
+
+    X = np.concatenate(X_all)
+    y = np.concatenate(y_all)
+    return X, y
+
+def get_label(filename):
+    if "rest" in filename:
+        return 0
+    elif "task_motor" in filename:
+        return 1
+    elif "task_story_math" in filename:
+        return 2
+    elif "task_working_memory" in filename:
+        return 3
+
+def create_window(matrix, window_size, stride):
+    windows = []
+    T = matrix.shape[1]
+
+    for start in range(0, T - window_size + 1, stride):
+        window = matrix[:, start:start + window_size]
+        windows.append(window.astype(np.float32))
+    return np.array(windows)
+
+def train_cnn(window_size, stride, number_epochs, batch_size, learning_rate, folder):
+    X_train, y_train = load_all_data(folder, window_size, stride)
+    X_train = tensor(X_train, dtype=torch.float32)
+    y_train = tensor(y_train, dtype=torch.long)
+    train_dataset = TensorDataset(X_train, y_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    model = CNN(
+        window_size=window_size,
+        conv_channels=[32, 64],
+        kernel_sizes=[7, 5]
+    )
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(number_epochs):
+        running_loss = 0.0
+        for i, (batch_x, batch_y) in enumerate(train_loader):
+            pred = model(batch_x)
+            loss = criterion(pred, batch_y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{number_epochs}], Loss: {running_loss/len(train_loader):.10f}")
+
+    return model
+
+def test_cnn(model, window_size, stride, batch_size, folder):
+    X_test, y_test = load_all_data(folder, window_size, stride)
+    X_test = tensor(X_test, dtype=torch.float32)
+    y_test = tensor(y_test, dtype=torch.long)
+    test_dataset = TensorDataset(X_test, y_test)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    acc = Accuracy(task="multiclass",num_classes=4)
+    model.eval()
+    with torch.no_grad():
+        for batch_x, batch_y in test_loader:
+            _, preds = torch.max(model(batch_x), 1)
+            acc.update(preds, batch_y)
+
+    test_acc = acc.compute()
+    print(f"Test Accuracy: {test_acc}")
--- Assign2/src/main.py
+++ Assign2/src/main.py
@@ -1,8 +1,20 @@
 import argparse
-
+from pathlib import Path
+from cnn_methods import train_cnn, test_cnn
 from utils.dirs import ClassificationType, INTRA_SPLITS, CROSS_SPLITS
 from preprocess import pre_process_data
 
+WINDOW_SIZE = 500
+STRIDE = 400
+NUMBER_EPOCHS = 10
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-3
+INTRA_TRAIN_FOLDER = Path("preprocessed_data/10/Intra/train")
+INTRA_TEST_FOLDER = Path("preprocessed_data/10/Intra/test")
+CROSS_TRAIN_FOLDER = Path("preprocessed_data/10/Cross/train")
+CROSS_TEST1_FOLDER = Path("preprocessed_data/10/Cross/test1")
+CROSS_TEST2_FOLDER = Path("preprocessed_data/10/Cross/test2")
+CROSS_TEST3_FOLDER = Path("preprocessed_data/10/Cross/test3")
 
 def main(classification_type: ClassificationType):
     if classification_type == ClassificationType.INTRA:
@@ -15,6 +27,17 @@ def main(classification_type: ClassificationType):
         print(f"Pre-processing {split.value} split...")
         pre_process_data(classification_type, split)
 
+    if classification_type == ClassificationType.INTRA:
+        model = train_cnn(WINDOW_SIZE, STRIDE, NUMBER_EPOCHS, BATCH_SIZE, LEARNING_RATE, INTRA_TRAIN_FOLDER)
+        test_cnn(model, WINDOW_SIZE, STRIDE, BATCH_SIZE, INTRA_TEST_FOLDER)
+    else:
+        model = train_cnn(WINDOW_SIZE, STRIDE, NUMBER_EPOCHS, BATCH_SIZE, LEARNING_RATE, CROSS_TRAIN_FOLDER)
+        print("test1 results")
+        test_cnn(model, WINDOW_SIZE, STRIDE, BATCH_SIZE, CROSS_TEST1_FOLDER)
+        print("test2 results")
+        test_cnn(model, WINDOW_SIZE, STRIDE, BATCH_SIZE, CROSS_TEST2_FOLDER)
+        print("test3 results")
+        test_cnn(model, WINDOW_SIZE, STRIDE, BATCH_SIZE, CROSS_TEST3_FOLDER)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MEG classification model.")
